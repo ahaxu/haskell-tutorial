cacb289cef8692d260227af14eb17624d7290f3c
longka <nguyentienlong88@gmail.com>
2022-03-28 07:02:51 +0700

    Add type class haskell

--- overview/2_type_class.hs
+++ overview/2_type_class.hs
@@ -7,12 +7,23 @@ data Shape =
       Circle Float
     | Rectangle Float Float -- pipe |
     | Square Float
-
 instance Show Shape where
-    show = _todo
+    show (Circle r) = "HT r=" ++ show r
+    show (Rectangle a b) = "HCN a=" ++ show a ++ " b=" ++ show b
+    show (Square a) = "HV a=" ++ show a
 
 -- pattern matching
 getArea :: Shape -> Float
 getArea (Circle r) = 3.14*r*r
 getArea (Rectangle a b) = a * b
 getArea (Square a) = a*a
+
+
+data XXX  a =
+      Nothing'
+    | XXX a
+    deriving Show
+
+instance Functor XXX where
+    fmap ab Nothing' = Nothing'
+    fmap ab (XXX a) = XXX $ ab a
